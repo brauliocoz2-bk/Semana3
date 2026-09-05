0a966c3f7fe61d43cca2ced0f8daff275df0fee3
laboratorioesan <laboratorioesan@esan.edu.pe>
2026-09-05 17:14:55 -0500

    Initial commit

---
+++ app/src/main/java/dev/braulio/esan/MainActivity.kt
@@ -0,0 +1,76 @@
+package dev.braulio.esan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.braulio.esan.ui.theme.Semana3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloComposeForm()
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HelloComposeForm(){
+    var name  by remember { mutableStateOf("") }
+    var birthDate by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {Text("Hola ESAN")})
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues = padding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+
+        )  {
+            Text("Bienvenido a Jetpack Compose")
+            OutlinedTextField(
+                value = name,
+                onValueChange = {name = it},
+                label = {Text("Nombre")}
+            )
+            OutlinedTextField(
+                value = birthDate,
+                onValueChange = {birthDate = it},
+                label = {Text("Fecha de nacimiento")}
+            )
+            Button(
+                onClick = {},
+                enabled = name.isNotEmpty() && birthDate.isNotEmpty()
+            ){
+                Text("Enviar")
+            }
+        }
+    }
+
+}
